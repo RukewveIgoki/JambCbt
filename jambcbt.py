b9442127ccbb6223fff0e2a0ccbd73c72cd6a871
Rukewve Emmanuel Igoki <144626728+RukewveIgoki@users.noreply.github.com>
2023-09-19 15:04:00 -0400

    Add files via upload

---
+++ jambcbt.py
@@ -0,0 +1,218 @@
+print("Welcome to jamb Cbt center")
+print("")
+print("1. Computer Science")
+print("2. International Relation")
+print("3. Mass Communication")
+
+print("")
+enter = input("Input Course Number: ")
+if enter == "1":
+    print("")
+    print("Question 1")
+if enter == "1":
+           q1 = input("what is another name for a motherboard: ")
+if enter == "1":
+           if q1 == "cpu":
+              if enter == "1":
+                print("correct, next question")
+           else:
+              if enter == "1":
+                print("wrong, next question")
+
+if enter == "1":
+  print("")
+  print("Question 2")
+if enter == "1":
+           q2 = input("What is the name of the python IDE used in this project: ")
+if enter == "1":
+            if q2 == "vscode":
+               if enter == "1":
+                 print('correct, next question')
+            else:
+              if enter == "1":
+                 print("wrong, next question")  
+
+if enter == "1":
+    print("")
+    print("Question 3")
+if enter == "1":
+        q3 = input("Another name for a small computer is: ")
+if enter == "1":
+           if q3 == "laptop":
+              if enter == "1":
+                print("correct, next question")
+           else:
+              if enter == "1":
+                print("wrong, next question")
+
+if enter == "1":
+   print("")
+   print("Question 4")
+if enter == "1":
+        q4 = input("what is the screen called in the computer: ")
+if enter == "1":
+           if q4 == "monitor":
+              if enter == "1":
+                print("correct, next question")
+           else:
+              if enter == "1":
+                print("wrong, next question") 
+
+if enter == "1":
+    print("")
+    print("Question 5")
+if enter == "1":
+        q5 = input("A device that has wire or is wireless that is used to control the computer cursor is: ")
+if enter == "1":
+           if q5 == "mouse":
+              if enter == "1":
+                print("correct, next question")
+           else:
+              if enter == "1":
+                print("wrong, next question")
+
+
+if enter == "2":
+ print("International Relation")
+if enter == "2":
+  print("")
+
+if enter == "2":
+    print("")
+    print("Question 1")
+if enter == "2":
+        q9 = input("Is international staudies in the humanities? : ")
+if enter == "2":
+           if q9 == "yes":
+              if enter == "2":
+                print("correct, next question")
+           else:
+              if enter == "2":
+                print("wrong, next question")
+
+if enter == "2":
+    print("")
+    print("Question 2")
+if enter == "2":
+        q10 = input("The council have how many Permanent members? : ")
+if enter == "2":
+           if q10 == "5":
+              if enter == "2":
+                print("correct, next question")
+           else:
+              if enter == "2":
+                print("wrong, next question")
+
+if enter == "2":
+    print("")
+    print("Question 3")
+if enter == "2":
+        q11 = input("When did international relations start? : ")
+if enter == "2":
+           if q11 == "1919":
+              if enter == "2":
+                print("correct, next question")
+           else:
+              if enter == "2":
+                print("wrong, next question")
+
+if enter == "2":
+    print("")
+    print("Question 4")
+if enter == "2":
+        q12 = input("when was National Front founded? : ")
+if enter == "2":
+           if q12 == "1967":
+              if enter == "2":
+                print("correct, next question")
+           else:
+              if enter == "2":
+                print("wrong, next question")
+
+if enter == "2":
+    print("")
+    print("Question 5")
+if enter == "2":
+        q13 = input("When was the Ba'ath Party founded? : ")
+if enter == "2":
+           if q13 == "1947":
+              if enter == "2":
+                print("correct, next question")
+           else:
+              if enter == "2":
+                print("wrong, next question")
+
+                
+                
+if enter == "3":
+ print("Mass Communication")
+if enter == "3":
+  print("")
+
+if enter == "3":
+    print("")
+    print("Question 1")
+if enter == "3":
+        q14 = input("What was the first tool of mass communication? : ")
+if enter == "3":
+           if q14 == "newspaper":
+              if enter == "3":
+                print("correct, next question")
+           else:
+              if enter == "3":
+                print("wrong, next question")
+
+if enter == "3":
+    print("")
+    print("Question 2")
+if enter == "3":
+        q15 = input("Year they came up with the definition of mass media? : ")
+if enter == "3":
+           if q15 == "1920":
+              if enter == "3":
+                print("correct, next question")
+           else:
+              if enter == "3":
+                print("wrong, next question")                
+
+if enter == "3":
+    print("")
+    print("Question 3")
+if enter == "3":
+        q15 = input("The liberal model of media and politics, according to Hallin and Mancini, is located in? : ")
+if enter == "3":
+           if q15 == "canada":
+              if enter == "3":
+                print("correct, next question")
+           else:
+              if enter == "3":
+                print("wrong, next question") 
+
+
+if enter == "3":
+    print("")
+    print("Question 4")
+if enter == "3":
+        q15 = input("Semiotic analysis regards media texts as a collection of? : ")
+if enter == "3":
+           if q15 == "paradigms":
+              if enter == "3":
+                print("correct, next question")
+           else:
+              if enter == "3":
+                print("wrong, next question")
+
+
+
+if enter == "3":
+    print("")
+    print("Question 5")
+if enter == "3":
+        q15 = input("Gatekeeping in newspapers is usually associated with? : ")
+if enter == "3":
+           if q15 == "news":
+              if enter == "3":
+                print("correct, next question")
+           else:
+              if enter == "3":
+                print("wrong, next question")
